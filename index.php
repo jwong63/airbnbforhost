9a5564c067fbcbfb8da995214124c4b120777a57
jwong63 <jackie.c.wong1@gmail.com>
2017-10-23 18:45:48 -0500

    First commit testing parse
    Using a java library api d3 to parse information from excel files to console

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Airbnb New Host</title>
+  </head>
+  <body>
+    <dl id="priceByNeighborhood">
+      <dt>Analysis Average Price by Neighborhood</dt>
+    </dl>
+  </body>
+</html>
---
+++ readingData.js
@@ -0,0 +1,5 @@
+<script src="https://d3js.org/d3.v4.js">
+  d3.csv("neighborhoods.csv", function(data)){
+    console.log(data[0]);
+  }
+</script>
